240877dd6045091c26a10083beb89a7f933c6480
NRAlejandro <nicolas.ramirez.alejandro@gmail.com>
2021-05-16 23:36:33 -0300

    Close #15

---
+++ src/main/java/com/nralejandro/springblog/model/PostModel.java
@@ -0,0 +1,34 @@
+package com.nralejandro.springblog.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostModel {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String body;
+
+    @Column
+    private Date timestamp;
+
+
+
+
+}
